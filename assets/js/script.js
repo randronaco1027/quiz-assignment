cc176ccbdb854805712638bbeddf88be5f669103
randronaco <randronaco1027@gmail.com>
2022-03-19 13:33:44 -0400

    added full question sequence, logged high score

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,97 @@
-// var startTimer = document.querySelector(".start-button");
+
 var timerEl = document.getElementById('timer');
 
 document.getElementById("generate").addEventListener("click", countdown);
-document.getElementById("timeless").addEventListener("click", reduceTime);
+document.getElementById("q1").style.display = "none";
+document.getElementById("q2").style.display = "none";
+document.getElementById("q3").style.display = "none";
+document.getElementById("q4").style.display = "none";
+document.getElementById("finalScoreName").style.display = "none";
 
 var timeLeft = 50;
 function countdown() {
-
-    // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
     var timeInterval = setInterval(function () {
-        // As long as the `timeLeft` is greater than 1
         if (timeLeft > 0) {
-            // Set the `textContent` of `timerEl` to show the remaining seconds
             timerEl.textContent = timeLeft + ' seconds remaining';
-            // Decrement `timeLeft` by 1
             timeLeft--;
         } else {
-            // Once `timeLeft` gets to 0, set `timerEl` to an empty string
             timerEl.textContent = "Time's up!";
-            // Use `clearInterval()` to stop the timer
             clearInterval(timeInterval);
-            // Call the `displayMessage()` function
         }
     }, 1000);
+    question1();
 }
 
+function question1() {
+    document.getElementById("generate").style.display = "none";
+    document.getElementById("countdown").textContent = "Question 1";
+    document.getElementById("q1").style.display = "block";
+    document.getElementById("q1").textContent = "Correct Answer";
+    document.getElementById("q1").addEventListener("click", question2);
+    // document.getElementById("generate").addEventListener("click", countdown);
+    document.getElementById("q2").style.display = "block";
+    document.getElementById("q2").textContent = "Wrong Answer";
+    document.getElementById("q2").addEventListener("click", reduceTime);
+    document.getElementById("q2").addEventListener("click", question2);
+    document.getElementById("q3").style.display = "block";
+    document.getElementById("q3").textContent = "Wrong Answer";
+    document.getElementById("q3").addEventListener("click", reduceTime);
+    document.getElementById("q3").addEventListener("click", question2);
+    document.getElementById("q4").style.display = "block";
+    document.getElementById("q4").textContent = "Wrong Answer";
+    document.getElementById("q4").addEventListener("click", reduceTime);
+    document.getElementById("q4").addEventListener("click", question2);
+}
 function reduceTime() {
     timeLeft -= 10;
 }
 
+function question2 () {
+    document.getElementById("countdown").textContent = "Question 2";
+    document.getElementById("q1").textContent = "Wrong Answer";
+    document.getElementById("q1").addEventListener("click", reduceTime);
+    document.getElementById("q1").addEventListener("click", question3);
+    document.getElementById("q2").textContent = "Wrong Answer";
+    document.getElementById("q2").addEventListener("click", reduceTime);
+    document.getElementById("q2").addEventListener("click", question3);
+    //This still reduces time, need to fix
+    document.getElementById("q3").textContent = "Correct Answer";
+    document.getElementById("q3").addEventListener("click", question3);
+    document.getElementById("q4").textContent = "Wrong Answer";
+    document.getElementById("q4").addEventListener("click", reduceTime);
+    document.getElementById("q4").addEventListener("click", question3);
+}
 
+function question3() {
+    document.getElementById("countdown").textContent = "Question 3";
+    document.getElementById("q1").textContent = "Wrong Answer";
+    document.getElementById("q1").addEventListener("click", reduceTime);
+    document.getElementById("q1").addEventListener("click", gameOver);
+    //This still reduces time, need to fix
+    document.getElementById("q2").textContent = "Correct Answer";
+    document.getElementById("q2").addEventListener("click", gameOver);
+    document.getElementById("q3").textContent = "Wrong Answer";
+    document.getElementById("q3").addEventListener("click", reduceTime);
+    document.getElementById("q3").addEventListener("click", gameOver);
+    document.getElementById("q4").textContent = "Wrong Answer";
+    document.getElementById("q4").addEventListener("click", reduceTime);
+    document.getElementById("q4").addEventListener("click", gameOver);
+}
 
-// document.getElementById("generate").addEventListener("click", myFunction);
-
-// function myFunction() {
-//   alert ("Hello World!");
-// }
+function addPoints() {
+    points =+ 10;
+}
+function gameOver() {
+    //Needs to be defined
+    // clearInterval(timeInterval);
+    document.getElementById("countdown").textContent = "Game Over! Final score: " + timeLeft;
+    document.getElementById("q1").style.display = "none";
+    document.getElementById("q2").style.display = "none";
+    document.getElementById("q3").style.display = "none";
+    document.getElementById("q4").style.display = "none";
+    timerEl.textContent = "";
+    console.log(timeLeft);
+    document.getElementById("scoreLabel").textContent = "Enter Initials: ";
+    document.getElementById("finalScoreName").textContent = "Enter Initials";
+    document.getElementById("finalScoreName").style.display = "visible";
+}
