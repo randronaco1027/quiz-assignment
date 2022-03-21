f5b1414d7954ca7dcdea7eac277e1d549b5a4990
randronaco <randronaco1027@gmail.com>
2022-03-21 11:54:55 -0400

    updated correct and wrong answer functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,6 +17,7 @@ function countdown() {
         } else {
             timerEl.textContent = "Time's up!";
             clearInterval(timeInterval);
+            gameOver();
         }
     }, 1000);
     question1();
@@ -28,59 +29,71 @@ function question1() {
     document.getElementById("q1").style.display = "block";
     document.getElementById("q1").textContent = "Correct Answer";
     document.getElementById("q1").addEventListener("click", question2);
-    // document.getElementById("generate").addEventListener("click", countdown);
+    document.getElementById("q1").addEventListener("click", correctAnswer);
     document.getElementById("q2").style.display = "block";
     document.getElementById("q2").textContent = "Wrong Answer";
-    document.getElementById("q2").addEventListener("click", reduceTime);
+    document.getElementById("q2").addEventListener("click", wrongAnswer);
     document.getElementById("q2").addEventListener("click", question2);
     document.getElementById("q3").style.display = "block";
     document.getElementById("q3").textContent = "Wrong Answer";
-    document.getElementById("q3").addEventListener("click", reduceTime);
+    document.getElementById("q3").addEventListener("click", wrongAnswer);
     document.getElementById("q3").addEventListener("click", question2);
     document.getElementById("q4").style.display = "block";
     document.getElementById("q4").textContent = "Wrong Answer";
-    document.getElementById("q4").addEventListener("click", reduceTime);
+    document.getElementById("q4").addEventListener("click", wrongAnswer);
     document.getElementById("q4").addEventListener("click", question2);
 }
-function reduceTime() {
-    timeLeft -= 10;
-}
 
-function question2 () {
+// Why does every button reduce time?
+function question2() {
     document.getElementById("countdown").textContent = "Question 2";
     document.getElementById("q1").textContent = "Wrong Answer";
-    document.getElementById("q1").addEventListener("click", reduceTime);
+    document.getElementById("q1").addEventListener("click", wrongAnswer);
     document.getElementById("q1").addEventListener("click", question3);
     document.getElementById("q2").textContent = "Wrong Answer";
-    document.getElementById("q2").addEventListener("click", reduceTime);
+    document.getElementById("q2").addEventListener("click", wrongAnswer);
     document.getElementById("q2").addEventListener("click", question3);
     //This still reduces time, need to fix
     document.getElementById("q3").textContent = "Correct Answer";
+    document.getElementById("q3").addEventListener("click", correctAnswer);
     document.getElementById("q3").addEventListener("click", question3);
     document.getElementById("q4").textContent = "Wrong Answer";
-    document.getElementById("q4").addEventListener("click", reduceTime);
+    document.getElementById("q4").addEventListener("click", wrongAnswer);
     document.getElementById("q4").addEventListener("click", question3);
 }
 
 function question3() {
     document.getElementById("countdown").textContent = "Question 3";
     document.getElementById("q1").textContent = "Wrong Answer";
-    document.getElementById("q1").addEventListener("click", reduceTime);
+    document.getElementById("q1").addEventListener("click", wrongAnswer);
     document.getElementById("q1").addEventListener("click", gameOver);
     //This still reduces time, need to fix
     document.getElementById("q2").textContent = "Correct Answer";
     document.getElementById("q2").addEventListener("click", gameOver);
+    document.getElementById("q2").addEventListener("click", correctAnswer);
     document.getElementById("q3").textContent = "Wrong Answer";
-    document.getElementById("q3").addEventListener("click", reduceTime);
+    document.getElementById("q3").addEventListener("click", wrongAnswer);
     document.getElementById("q3").addEventListener("click", gameOver);
     document.getElementById("q4").textContent = "Wrong Answer";
-    document.getElementById("q4").addEventListener("click", reduceTime);
+    document.getElementById("q4").addEventListener("click", wrongAnswer);
     document.getElementById("q4").addEventListener("click", gameOver);
 }
 
-function addPoints() {
-    points =+ 10;
+function wrongAnswer() {
+    timeLeft -= 10;
+    document.getElementById("answer").textContent = "Wrong!"
+    var wrongAnswerInterval = setInterval(function () {
+        document.getElementById("answer").textContent = ""
+    }, 1000)
 }
+
+function correctAnswer() {
+    document.getElementById("answer").textContent = "Correct!"
+    var correctAnswerInterval = setInterval(function () {
+        document.getElementById("answer").textContent = ""
+    }, 1000)
+}
+
 function gameOver() {
     //Needs to be defined
     // clearInterval(timeInterval);
@@ -92,6 +105,7 @@ function gameOver() {
     timerEl.textContent = "";
     console.log(timeLeft);
     document.getElementById("scoreLabel").textContent = "Enter Initials: ";
-    document.getElementById("finalScoreName").textContent = "Enter Initials";
     document.getElementById("finalScoreName").style.display = "visible";
+    document.getElementById("finalScoreName").textContent = "Enter Initials";
+
 }
