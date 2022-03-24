bcf6553274eee83614f60b6bf0de504a5383a914
randronaco <randronaco1027@gmail.com>
2022-03-24 17:59:15 -0400

    merged arrays

--- assets/js/highscore.js
+++ assets/js/highscore.js
@@ -8,8 +8,15 @@ var initialScore = displayInitials.concat(displayScore)
 console.log(displayInitials)
 console.log(displayScore)
 console.log(initialScore)
+console.log(initialScore[0])
 
 document.getElementById("displayHighScore").textContent = initialScore;
+
+for (var i = 0; i < (initialScore.length / 2); i++) {
+    var index = (initialScore.length / 2)
+    console.log(initialScore[i], initialScore[i + index])
+    document.getElementById("displayHighScore").textContent = initialScore[i] + initialScore[i + index];
+}
 
 
 // document.getElementById("displayHighScore").textContent = JSON.parse(localStorage.getItem("newInitials"));
